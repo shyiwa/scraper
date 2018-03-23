c48ca2b79f53fec75a23e97739ebce0b875c049f
shyiwa <1289271081@qq.com>
2018-03-24 03:12:45 +0800

    修改

--- download_dmzx.py
+++ download_dmzx.py
@@ -5,7 +5,6 @@
 import urllib as ul
 import requests
 import os
-import re
 import itertools
 from datetime import datetime as dt
 from bs4 import BeautifulSoup
@@ -21,19 +20,19 @@ def download(url):
     nChapters = len(table)
     
     print('共', str(nChapters), '章')
-	start = dt.now()
+    start = dt.now()
         
 #    for j in range(nChapters):
     for chap in table:
-        print('下载', chap_name)
-		chap_url = chap['href']				# 章节的链接网址
+        chap_url = chap['href']				# 章节的链接网址
         chap_name = chap['title'].strip()	# 章节名
+        print('下载', chap_name)
         
-        path = os.path.join(os.path.curdir, name, chap_name)
-		if not os.path.exists(path):
-			os.makedirs(path) 				# 创建章节的文件夹
+        path = os.path.join(os.path.curdir, name, chap_name)		
+        if not os.path.exists(path):
+            os.makedirs(path) 				# 创建章节的文件夹
 
-		for i in itertools.count(1):
+        for i in itertools.count(1):
             page_url = chap_url + '#p=%d' % i		# 章节的页面网址
             path = os.path.join(os.path.curdir, name, chap_name, str(i)+'.jpg')
             browser.get(page_url)
@@ -46,8 +45,8 @@ def download(url):
             except Exception as e:
                 break	# 下载图片时出错说明当前章节的图片已经全部下载过
         
-			end = dt.now()
-			print((end-start).total_seconds(), '秒')
+            end = dt.now()
+            print((end-start).total_seconds(), '秒')
                 
 #显示下载进度
 def schedule(a,b,c):
@@ -59,11 +58,10 @@ def schedule(a,b,c):
 
 
 if __name__ == '__main__':
-    
-	# for i in range(92):
-       # path = './2/'+str(i+1)+'.jpg'
-       # url = 'http://183.91.33.78/p0.xiaoshidi.net/2013/01/29035807'+str(i)+'.jpg'
-       # ul.request.urlretrieve(url,path)
+    # for i in range(92):
+    # path = './2/'+str(i+1)+'.jpg'
+    # url = 'http://183.91.33.78/p0.xiaoshidi.net/2013/01/29035807'+str(i)+'.jpg'
+    # ul.request.urlretrieve(url,path)
     
     # chrome_options = Options()
     # chrome_options.add_argument('--headless')
@@ -71,8 +69,7 @@ if __name__ == '__main__':
     # chrome_options.binary_location = 'C:/Users/wys/AppData/Local/Google/Chrome/Application/chrome.exe'
 
     # browser = webdriver.Chrome(chrome_options = chrome_options)
-	browser = webdriver.PhantomJS(executable_path='C:/Users/WWW/Anaconda3/pkgs/phantomjs-2.1.1-0/Library/bin/PhantomJS')
-    browser.get(url)
+    browser = webdriver.PhantomJS(executable_path='C:/Users/WWW/Anaconda3/pkgs/phantomjs-2.1.1-0/Library/bin/PhantomJS')
     
 	# headers={
 	# 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER' 
